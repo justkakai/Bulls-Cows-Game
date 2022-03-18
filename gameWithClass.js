301e00db4b813cb073848b58aff084f41d73103f
Kakai <kenkai24@yahoo.com>
2022-03-18 23:02:23 +0100

    added methods

--- gameWithClass.js
+++ gameWithClass.js
@@ -3,6 +3,33 @@ const colors = require('colors');
 
 class BullsAndCows {
     constructor() {
+        this.numberLength = this.numberLength;
+    }
+    welcomeAndName() {
+
+    }
+    modeSelection() {
+
+    }
+    firstGuess() {
+
+    }
+    validation() {
+
+    }
+    result() {
+
+    }
+    exitOrContinue() {
+
+    }
+    calculation() {
+
+    }
+    displayRandomMessage() {
+
+    }
+    randomizedNumber() {
         
     }
 }
